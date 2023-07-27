c8cf11424814b84fbf8681079ff91b7c369f9452
root <root@0be5f4438cd9>
2023-07-27 02:53:25 +0000

    Still working on FortuneCookies

--- src/_03_gui_with_help/_5_duelling_buttons/DuellingButtons.java
+++ src/_03_gui_with_help/_5_duelling_buttons/DuellingButtons.java
@@ -27,21 +27,23 @@ public class DuellingButtons implements ActionListener {
 	public void run() {
 
 		// 1. Add the panel to the frame
-
+		frame.add(panel); 
 		// 2. Make the frame visible
-
+		frame.setVisible(true);
 		// 3. Set the text of the leftButton to "Click me!"
-
+		 leftButton.setText("Click me!");
 		// 4. Set the text of the rightButton to "Click me!"
-
+		 rightButton.setText("Click me!");
 		// 5. Add an action listener to the leftButton
-
+		 leftButton.addActionListener(this);
 		// 6. Add an action listener to the rightButton
-
+		 rightButton.addActionListener(this);
 		// 7. Add the leftButton to the panel
-
+		 panel.add(leftButton);
+		 panel.add(rightButton);
+		 frame.pack();
 		// 8. Add the rightButton to the panel
-
+		 frame.setTitle("Demanding Buttons");
 		// 9. Pack the frame
 
 		// 10. Set the title of the frame to "Demanding Buttons"
